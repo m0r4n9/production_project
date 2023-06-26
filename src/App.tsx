6b87cb881a8423a62ff81d91c47f98b4fd71d260
sashasevostyanov <sevostanovsasa7@gmail.com>
2023-06-26 12:25:08 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, {Suspense, useContext, useState} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+import './styles/index.scss';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+import {className} from "./helpers/classNames/className";
+
+
+
+const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
+    const bool = true;
+
+    return (
+        <div className={className('app', {}, [theme])}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,7 @@
+import {lazy} from "react";
+
+export const MainPageAsync = lazy(() => new Promise(resolve => {
+    // @ts-ignore
+    // Так в реальных проектах не делать, это для примера
+    setTimeout(() => resolve(import('./MainPage')), 1500)
+}));
