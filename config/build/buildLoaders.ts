e0ea30fe38e33f6a45e2cebafbb2a26aa49b1a65
sashasevostyanov <sevostanovsasa7@gmail.com>
2023-07-21 02:47:57 +0300

    configure babel for ts, add fork ts checker, add custom babel plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -12,16 +12,17 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     }
 
-    const babelLoader = buildBabelLoader(options);
+    const codeBabelLoader = buildBabelLoader({...options, isTsx: false});
+    const tsxCodeBabelLoader = buildBabelLoader({...options, isTsx: true});
 
     const cssLoader = buildCssLoader(isDev);
 
     // Если не используем TS - нужен babel-loader
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+    // const typescriptLoader = {
+    //     test: /\.tsx?$/,
+    //     use: 'ts-loader',
+    //     exclude: /node_modules/,
+    // }
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif)$/i,
@@ -34,9 +35,9 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 
     return [
         fileLoader,
-        babelLoader,
         svgLoader,
-        typescriptLoader,
+        codeBabelLoader,
+        tsxCodeBabelLoader,
         cssLoader,
     ]
 }
