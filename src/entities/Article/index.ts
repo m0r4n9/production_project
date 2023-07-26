f28b3e052172032870fdbc53496a3780480b55af
sashasevostyanov <sevostanovsasa7@gmail.com>
2023-07-26 19:25:49 +0300

    refactor entities and features

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -5,6 +5,3 @@ export {getArticleDetailsData} from './model/selectors/articleDetails'
 
 export {ArticleDetails} from './ui/ArticleDetails/ArticleDetails';
 export {ArticleList} from './ui/ArticleList/ArticleList';
-export {ArticleViewSelector} from './ui/ArticleViewSelector/ArticleViewSelector';
-export {ArticleSortSelector} from './ui/ArticleSortSelector/ArticleSortSelector';
-export {ArticleTypeTabs} from './ui/ArticleTypeTabs/ArticleTypeTabs';
--- src/pages/ArticlesPage/ui/ArticlePageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlePageFilters/ArticlesPageFilters.tsx
@@ -4,11 +4,8 @@ import {useTranslation} from "react-i18next"
 import {memo, useCallback} from 'react';
 import {
     ArticleSortField,
-    ArticleSortSelector,
     ArticleType,
-    ArticleTypeTabs,
-    ArticleView,
-    ArticleViewSelector
+    ArticleView
 } from "@/entities/Article";
 import {articlesPageActions} from "../../model/slice/articlesPageSlice";
 import {useAppDispatch} from "@/shared/lib/hooks/useAppDispatch/useAppDispatch";
@@ -25,6 +22,9 @@ import {Input} from "@/shared/ui/Input";
 import {SortOrder} from "@/shared/types";
 import {fetchArticlesList} from "../../model/services/fetchArticlesList/fetchArticlesList";
 import {useDebounce} from "@/shared/lib/hooks/useDebounce/useDebounce";
+import {ArticleTypeTabs} from "@/features/ArticleTypeTabs";
+import {ArticleViewSelector} from "@/features/ArticleViewSelector";
+import {ArticleSortSelector} from "@/features/ArticleSortSelector";
 
 interface ArticlePageFiltersProps {
     className?: string;
