d9f847191472dc8ff15e7fe3f527284cc7044072
sashasevostyanov <sevostanovsasa7@gmail.com>
2023-07-27 17:49:44 +0300

    init auth data through server

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,23 @@ import { AppRoute } from '@/app/providers/router';
 import { Navbar } from '@/widgets/Navbar';
 import { Sidebar } from '@/widgets/Sidebar';
 import { Suspense, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getUserInited, userActions } from '@/entities/User';
+import { useSelector } from 'react-redux';
+import { getUserInited, initAuthData } from '@/entities/User';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { PageLoader } from '@/widgets/PageLoader';
 
 const App = () => {
     const { theme } = useTheme();
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
     const inited = useSelector(getUserInited);
 
     useEffect(() => {
-        dispatch(userActions.initAuthData());
+        dispatch(initAuthData());
     }, [dispatch]);
+
+    if (!inited) {
+        return <PageLoader />;
+    }
 
     return (
         <div className={classNames('app', {}, [theme])}>
--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,8 +1,11 @@
-import {createAsyncThunk} from "@reduxjs/toolkit";
-import {User, userActions} from "@/entities/User";
-import i18n from "@/shared/config/i18n/i18n";
-import {USER_LOCALSTORAGE_KEY} from "@/shared/const/localstorage";
-import {ThunkConfig, ThunkExtraArg} from "@/app/providers/StoreProvider/config/StateSchema";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { User, userActions } from '@/entities/User';
+import i18n from '@/shared/config/i18n/i18n';
+import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
+import {
+    ThunkConfig,
+    ThunkExtraArg,
+} from '@/app/providers/StoreProvider/config/StateSchema';
 
 interface LoginByUsernameProps {
     username: string;
@@ -13,18 +16,16 @@ export const loginByUsername = createAsyncThunk<
     User,
     LoginByUsernameProps,
     ThunkConfig<string>
->(
-    'login/loginByUsername',
-    async (authData, thunkAPI) => {
-        const {extra, dispatch, rejectWithValue} = thunkAPI;
-        try {
-            const response = await extra.api.post<User>('/login', authData);
-            localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-            dispatch(userActions.setAuthData(response.data));
-            return response.data;
-        } catch (e) {
-            console.log(e);
-            return rejectWithValue(i18n.t('Вы ввели неправильный логин или пароль'));
-        }
+>('login/loginByUsername', async (authData, thunkAPI) => {
+    const { extra, dispatch, rejectWithValue } = thunkAPI;
+    try {
+        const response = await extra.api.post<User>('/login', authData);
+        dispatch(userActions.setAuthData(response.data));
+        return response.data;
+    } catch (e) {
+        console.log(e);
+        return rejectWithValue(
+            i18n.t('Вы ввели неправильный логин или пароль'),
+        );
     }
-)
+});
